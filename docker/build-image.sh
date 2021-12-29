58147594978f21676661f037cc9ffd0df2affe67
Alexander Reichert <alex@flecs.tech>
2021-12-29 14:20:10 +0100

    FLX-33: Provide local socket connection to communicate with service
    - restructure Debian packages & Docker images

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -13,10 +13,8 @@ DIRNAME=$(dirname $(readlink -f ${0}))/${IMAGE}
 
 echo "Building image ${IMAGE} in context ${DIRNAME}"
 mkdir -p ${DIRNAME}/build-utils
-cp -r $(git rev-parse --show-toplevel)/build-utils/docker ${DIRNAME}/build-utils/
-docker buildx build \
+cp -r $(git rev-parse --show-toplevel)/build/utils/docker ${DIRNAME}/build-utils/
+docker build \
 	--tag flecs/${IMAGE} \
-	--platform linux/amd64,linux/arm/v7,linux/arm64 \
-	--output=type=registry \
 	--file ${DIRNAME}/dockerfiles/Dockerfile.${IMAGE} ${DOCKER_ARGS} ${DIRNAME}
 rm -rf ${DIRNAME}/build-utils
