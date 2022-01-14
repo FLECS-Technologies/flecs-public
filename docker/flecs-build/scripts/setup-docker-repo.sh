7fee5d78b55c4adb9d97b328c08cfe6f104b1728
Alexander Reichert <alex@flecs.tech>
2022-01-14 09:51:02 +0100

    Update actions to use github secrets

---
+++ docker/flecs-build/scripts/setup-docker-repo.sh
@@ -0,0 +1,26 @@
+## Copyright 2021 FLECS Technologies GmbH
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+## http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
+#!/bin/bash
+
+run () {
+  $* || exit $?
+}
+
+
+run curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+run echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
