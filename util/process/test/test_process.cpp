22e53c0249d49c8e0e3dbcd88cd88719c7460ce1
Alexander Reichert <alex@flecs.tech>
2022-02-08 10:14:18 +0100

    util: Google Test for process

---
+++ util/process/test/test_process.cpp
@@ -0,0 +1,72 @@
+// Copyright 2021-2022 FLECS Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "gtest/gtest.h"
+#include "util/process/process.h"
+
+TEST(util_process, spawn)
+{
+    auto test_process = FLECS::process_t{};
+
+    const auto spawn_res = test_process.spawn("/bin/echo", "-n", "FLECS");
+    ASSERT_EQ(spawn_res, 0);
+
+    const auto wait_res = test_process.wait(false, false);
+    ASSERT_NE(wait_res, -1);
+
+    ASSERT_EQ(test_process.exit_code(), 0);
+    ASSERT_EQ(test_process.stdout(), "FLECS");
+}
+
+TEST(util_process, spawnp)
+{
+    auto test_process = FLECS::process_t{};
+
+    const auto spawn_res = test_process.spawnp("echo", "-n", "FLECS");
+    ASSERT_EQ(spawn_res, 0);
+
+    const auto wait_res = test_process.wait(false, false);
+    ASSERT_NE(wait_res, -1);
+
+    ASSERT_EQ(test_process.exit_code(), 0);
+    ASSERT_EQ(test_process.stdout(), "FLECS");
+}
+
+TEST(util_process, spawn_fail)
+{
+    auto test_process = FLECS::process_t{};
+
+    const auto spawn_res = test_process.spawnp("nonexistent-binary");
+    ASSERT_NE(spawn_res, 0);
+
+    const auto wait_res = test_process.wait(false, false);
+    ASSERT_EQ(wait_res, -1);
+
+    ASSERT_EQ(test_process.stdout(), "");
+    ASSERT_EQ(test_process.stderr(), "");
+}
+
+TEST(util_process, spawnp_fail)
+{
+    auto test_process = FLECS::process_t{};
+
+    const auto spawn_res = test_process.spawn("/this/path/does/not/exist");
+    ASSERT_NE(spawn_res, 0);
+
+    const auto wait_res = test_process.wait(false, false);
+    ASSERT_EQ(wait_res, -1);
+
+    ASSERT_EQ(test_process.stdout(), "");
+    ASSERT_EQ(test_process.stderr(), "");
+}
