6722cca8b846b23076eb9cf821ff3185e6c69319
Alexander Reichert <alex@flecs.tech>
2022-02-21 21:38:29 +0100

    Lib: moved to API

--- cli/src/main.cpp
+++ cli/src/main.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "daemon/lib/libflecs.h"
+#include "libflecs.h"
 
 int main(int argc, char** argv)
 {
