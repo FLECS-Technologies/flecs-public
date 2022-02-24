e43dfaefc68b7fd5688315d6ff377ae15bde9f40
Alexander Reichert <alex@flecs.tech>
2022-02-24 12:06:57 +0100

    Daemon: add system module/endpoint

---
+++ daemon/modules/system/src/system.cpp
@@ -0,0 +1,40 @@
+// Copyright 2021-2022 FLECS Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "system.h"
+
+#include <cstdio>
+
+#include "factory/factory.h"
+
+namespace FLECS {
+
+namespace {
+register_module_t<module_system_t> _reg("system");
+}
+
+module_system_t::module_system_t()
+{
+    using namespace std::placeholders;
+
+    api::register_endpoint("/system/ping", std::bind(&module_system_t::ping, this, _1, _2));
+}
+
+http_status_e module_system_t::ping(const Json::Value& /*args*/, Json::Value& response)
+{
+    response["additionalInfo"] = "OK";
+    return http_status_e::Ok;
+}
+
+} // namespace FLECS
---
+++ daemon/modules/system/system.h
@@ -0,0 +1,33 @@
+// Copyright 2021-2022 FLECS Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef FLECS_daemon_modules_system_h
+#define FLECS_daemon_modules_system_h
+
+#include "module_base/module.h"
+
+namespace FLECS {
+
+class module_system_t : public module_t
+{
+public:
+    module_system_t();
+
+private:
+    http_status_e ping(const Json::Value& args, Json::Value& response);
+};
+
+} // namespace FLECS
+
+#endif // FLECS_daemon_modules_system_h
