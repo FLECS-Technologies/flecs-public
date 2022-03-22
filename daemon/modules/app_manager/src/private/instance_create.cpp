966d59484c9499457ba3ac0bb2fbcb3c5f79608c
Alexander Reichert <alex@flecs.tech>
2022-03-22 21:32:23 +0100

    Daemon: Apply hostname when creating instances

--- daemon/modules/app_manager/src/private/instance_create.cpp
+++ daemon/modules/app_manager/src/private/instance_create.cpp
@@ -163,6 +163,12 @@ http_status_e module_app_manager_private_t::do_create_instance(
     docker_process.arg("--name");
     docker_process.arg("flecs-" + hex_id);
 
+    if (!app.hostname().empty())
+    {
+        docker_process.arg("--hostname");
+        docker_process.arg(app.hostname());
+    }
+
     // assign static ip
     const auto ip = generate_instance_ip();
     docker_process.arg("--ip");
