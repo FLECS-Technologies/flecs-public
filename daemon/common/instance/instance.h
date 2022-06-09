61510a7bbe8fa3f57c012a2c0a22a86de34cab9a
Alexander Reichert <alex@flecs.tech>
2022-06-09 10:14:36 +0200

    daemon: implement instance class

---
+++ daemon/common/instance/instance.h
@@ -0,0 +1,141 @@
+// Copyright 2021-2022 FLECS Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef BFBA07F5_D230_427A_814B_DA012423C246
+#define BFBA07F5_D230_427A_814B_DA012423C246
+
+#include <memory>
+#include <string>
+
+#include "instance_config.h"
+#include "instance_status.h"
+
+namespace FLECS {
+
+class instance_t
+{
+public:
+    instance_t(std::string app, std::string version, instance_status_e status, instance_status_e desired);
+
+    instance_t(
+        std::string id,
+        std::string app,
+        std::string version,
+        std::string description,
+        instance_status_e status,
+        instance_status_e desired);
+
+    auto id() const noexcept //
+        -> const std::string&;
+    auto app() const noexcept //
+        -> const std::string&;
+    auto version() const noexcept //
+        -> const std::string&;
+    auto description() const noexcept //
+        -> const std::string&;
+    auto status() const noexcept //
+        -> instance_status_e;
+    auto desired() const noexcept //
+        -> instance_status_e;
+    auto config() const noexcept //
+        -> const instance_config_t&;
+    auto config() noexcept //
+        -> instance_config_t&;
+
+    auto regenerate_id() //
+        -> void;
+    auto description(std::string description) //
+        -> void;
+    auto status(instance_status_e instance_status) //
+        -> void;
+    auto desired(instance_status_e instance_status) //
+        -> void;
+
+private:
+    std::string _id;
+    std::string _app;
+    std::string _version;
+    std::string _description;
+    instance_status_e _status;
+    instance_status_e _desired;
+    instance_config_t _config;
+};
+
+inline auto instance_t::id() const noexcept //
+    -> const std::string&
+{
+    return _id;
+}
+
+inline auto instance_t::app() const noexcept //
+    -> const std::string&
+{
+    return _app;
+}
+inline auto instance_t::version() const noexcept //
+    -> const std::string&
+{
+    return _version;
+}
+
+inline auto instance_t::description() const noexcept //
+    -> const std::string&
+{
+    return _description;
+}
+
+inline auto instance_t::status() const noexcept //
+    -> instance_status_e
+{
+    return _status;
+}
+
+inline auto instance_t::desired() const noexcept //
+    -> instance_status_e
+{
+    return _desired;
+}
+
+inline auto instance_t::config() noexcept //
+    -> instance_config_t&
+{
+    return _config;
+}
+
+inline auto instance_t::config() const noexcept //
+    -> const instance_config_t&
+{
+    return _config;
+}
+
+inline auto instance_t::description(std::string description) //
+    -> void
+{
+    _description = description;
+}
+inline auto instance_t::status(instance_status_e status) //
+    -> void
+{
+    _status = status;
+}
+
+inline auto instance_t::desired(instance_status_e desired) //
+    -> void
+{
+    _desired = desired;
+}
+
+} // namespace FLECS
+
+#endif // BFBA07F5_D230_427A_814B_DA012423C246
---
+++ daemon/common/instance/src/instance.cpp
@@ -0,0 +1,63 @@
+// Copyright 2021-2022 FLECS Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "instance.h"
+
+#include <cstdio>
+#include <random>
+
+namespace FLECS {
+
+std::string generate_instance_id()
+{
+    auto res = std::string(8, '\0');
+
+    auto seed = std::random_device{};
+    auto generator = std::mt19937{seed()};
+    auto distribution = std::uniform_int_distribution{
+        std::numeric_limits<std::uint32_t>::min(),
+        std::numeric_limits<std::uint32_t>::max()};
+
+    auto id = distribution(generator);
+    std::snprintf(res.data(), res.length() + 1, "%.8x", id);
+
+    return res;
+}
+
+instance_t::instance_t(std::string app, std::string version, instance_status_e status, instance_status_e desired)
+    : instance_t{generate_instance_id(), app, version, std::string{}, status, desired}
+{}
+
+instance_t::instance_t(
+    std::string id,
+    std::string app,
+    std::string version,
+    std::string description,
+    instance_status_e status,
+    instance_status_e desired)
+    : _id{id}
+    , _app{app}
+    , _version{version}
+    , _description{description}
+    , _status{status}
+    , _desired{desired}
+{}
+
+auto instance_t::regenerate_id() //
+    -> void
+{
+    _id = generate_instance_id();
+}
+
+} // namespace FLECS
