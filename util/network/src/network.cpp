0f72c2ab9d6d6c5727eec4cfda2db9c59a12e198
Alexander Reichert <alex@flecs.tech>
2022-06-11 19:43:46 +0200

    util: extend network utils

--- util/network/src/network.cpp
+++ util/network/src/network.cpp
@@ -14,7 +14,10 @@
 
 #include "network.h"
 
+#include <arpa/inet.h>
+
 #include <bitset>
+#include <regex>
 
 #include "util/string/string_utils.h"
 
@@ -52,14 +55,38 @@ auto ipv6_to_string(const in6_addr& ip) //
     return std::string{buf};
 }
 
-auto subnet_to_cidr_v4(const std::string& subnet_mask) //
+auto subnet_mask_to_cidr_v4(std::string_view subnet_mask) //
     -> std::size_t
 {
-    auto subnet_bits = std::bitset<8 * sizeof(in_addr_t)>{ipv4_to_bits(subnet_mask).s_addr};
+    const auto subnet_bits = std::bitset<8 * sizeof(in_addr_t)>{ipv4_to_bits(subnet_mask).s_addr};
     return subnet_bits.count();
 }
 
-auto ipv4_to_network(const std::string& ip, const std::string& subnet_mask) //
+auto cidr_to_subnet_mask_v4(std::string_view cidr_subnet) //
+    -> std::string
+{
+    // until end of line: d/[0-32]
+    // e.g. 127.0.0.1/24 -> 1/24
+    const auto subnet_regex = std::regex{R"(\d\/([0-9]|[1][0-9]|[2][0-9]|[3][0-2])$)"};
+    auto m = std::cmatch{};
+    if (!std::regex_search(cidr_subnet.data(), m, subnet_regex) || m.size() < 2)
+    {
+        return std::string{};
+    }
+
+    const auto subnet_size = std::stoi(m[1].str());
+    auto subnet_bits = std::bitset<8 * sizeof(in_addr_t)>{};
+    for (auto i = 0; i < subnet_size; ++i)
+    {
+        subnet_bits.set(subnet_bits.size() - i - 1);
+    }
+
+    const auto addr = in_addr{.s_addr = static_cast<in_addr_t>(subnet_bits.to_ulong())};
+
+    return ipv4_to_string(addr);
+}
+
+auto ipv4_to_network(std::string_view ip, std::string_view subnet_mask) //
     -> std::string
 {
     const auto ip_addr = ipv4_to_bits(ip).s_addr;
@@ -67,7 +94,7 @@ auto ipv4_to_network(const std::string& ip, const std::string& subnet_mask) //
 
     const auto network_addr = in_addr{.s_addr = (ip_addr & subnet_addr)};
 
-    return ipv4_to_string(network_addr) + "/" + stringify(subnet_to_cidr_v4(subnet_mask));
+    return ipv4_to_string(network_addr) + "/" + stringify(subnet_mask_to_cidr_v4(subnet_mask));
 }
 
 } // namespace FLECS
