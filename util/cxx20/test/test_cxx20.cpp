99f1f0eb62848d6417b19567b4c6ffedaeaee7eb
Alexander Reichert <alex@flecs.tech>
2022-06-20 21:43:06 +0200

    util: add test for cxx20 utils

---
+++ util/cxx20/test/test_cxx20.cpp
@@ -0,0 +1,54 @@
+// Copyright 2021-2022 FLECS Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "gtest/gtest.h"
+#include "util/cxx20/string.h"
+
+TEST(cxx20, string)
+{
+    using std::operator""s;
+    using std::operator""sv;
+
+    const auto s = "This is a string";
+    const auto str = "This is a string"s;
+    const auto sv = "This is a string"sv;
+
+    ASSERT_TRUE(FLECS::cxx20::contains(s, "This"));
+    ASSERT_TRUE(FLECS::cxx20::contains(str, "This"));
+    ASSERT_TRUE(FLECS::cxx20::contains(sv, "This"));
+
+    ASSERT_TRUE(FLECS::cxx20::contains(s, "string"sv));
+    ASSERT_TRUE(FLECS::cxx20::contains(str, "string"sv));
+    ASSERT_TRUE(FLECS::cxx20::contains(sv, "string"sv));
+
+    ASSERT_FALSE(FLECS::cxx20::contains(s, "flecs"));
+    ASSERT_FALSE(FLECS::cxx20::contains(str, "flecs"));
+    ASSERT_FALSE(FLECS::cxx20::contains(sv, "flecs"));
+
+    ASSERT_FALSE(FLECS::cxx20::contains(s, "123"sv));
+    ASSERT_FALSE(FLECS::cxx20::contains(str, "123"sv));
+    ASSERT_FALSE(FLECS::cxx20::contains(sv, "123"sv));
+
+    ASSERT_TRUE(FLECS::cxx20::starts_with(s, "This"));
+    ASSERT_TRUE(FLECS::cxx20::starts_with(str, "This"));
+    ASSERT_TRUE(FLECS::cxx20::starts_with(sv, "This"));
+
+    ASSERT_TRUE(FLECS::cxx20::starts_with(s, "This"sv));
+    ASSERT_TRUE(FLECS::cxx20::starts_with(str, "This"sv));
+    ASSERT_TRUE(FLECS::cxx20::starts_with(sv, "This"sv));
+
+    ASSERT_TRUE(FLECS::cxx20::starts_with(s, 'T'));
+    ASSERT_TRUE(FLECS::cxx20::starts_with(str, 'T'));
+    ASSERT_TRUE(FLECS::cxx20::starts_with(sv, 'T'));
+}
