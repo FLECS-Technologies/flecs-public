dc2c3085d4edeadd5baeb0993467a38731c29e89
Alexander Reichert <alex@flecs.tech>
2022-07-03 16:44:58 +0200

    util: add wrapper class for libudev

---
+++ util/udev/src/udev.cpp
@@ -0,0 +1,82 @@
+// Copyright 2021-2022 FLECS Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "udev.h"
+
+#include <libudev.h>
+
+#include <utility>
+
+namespace FLECS {
+namespace udev {
+
+udev_t::udev_t()
+    : _handle{udev_new()}
+    , _owner{std::this_thread::get_id()}
+{}
+
+udev_t::udev_t(const udev_t& other)
+    : _handle{udev_ref(*other)}
+    , _owner{other._owner}
+{
+    validate_owner();
+}
+
+udev_t::udev_t(udev_t&& other)
+    : _handle{std::move(other._handle)}
+    , _owner{std::move(other._owner)}
+{
+    other._handle = nullptr;
+    other._owner = std::thread::id{};
+
+    validate_owner();
+}
+
+udev_t& udev_t::operator=(udev_t other)
+{
+    swap(*this, other);
+    return *this;
+}
+
+udev_t::~udev_t()
+{
+    if (_handle)
+    {
+        validate_owner();
+        udev_unref(_handle);
+    }
+}
+
+auto swap(udev_t& lhs, udev_t& rhs) //
+    -> void
+{
+    lhs.validate_owner();
+    rhs.validate_owner();
+
+    using std::swap;
+    swap(lhs._handle, rhs._handle);
+    swap(lhs._owner, rhs._owner);
+}
+
+auto udev_t::validate_owner() const //
+    -> void
+{
+    if (_owner != std::this_thread::get_id())
+    {
+        throw std::runtime_error{"Cannot re-use udev handle in different thread"};
+    }
+}
+
+} // namespace udev
+} // namespace FLECS
---
+++ util/udev/udev.h
@@ -0,0 +1,67 @@
+// Copyright 2021-2022 FLECS Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CA521E3A_3058_404F_822B_B77700F2CA78
+#define CA521E3A_3058_404F_822B_B77700F2CA78
+
+#include <stdexcept>
+#include <thread>
+
+struct udev;
+
+namespace FLECS {
+namespace udev {
+
+class udev_t
+{
+public:
+    using value_type = ::udev*;
+
+    udev_t();
+
+    udev_t& operator=(udev_t other);
+
+    udev_t(const udev_t& other);
+
+    udev_t(udev_t&& other);
+
+    ~udev_t();
+
+    auto operator*() noexcept //
+        -> value_type&
+    {
+        return _handle;
+    }
+
+    auto operator*() const noexcept //
+        -> const value_type&
+    {
+        return _handle;
+    }
+
+private:
+    friend auto swap(udev_t& lhs, udev_t& rhs) //
+        -> void;
+
+    auto validate_owner() const //
+        -> void;
+
+    value_type _handle;
+    std::thread::id _owner;
+};
+
+} // namespace udev
+} // namespace FLECS
+
+#endif /* CA521E3A_3058_404F_822B_B77700F2CA78 */
