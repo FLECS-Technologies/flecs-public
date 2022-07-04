cbeabd095fab40ae0bd7209a308c91a357072102
Alexander Reichert <alex@flecs.tech>
2022-07-04 13:48:15 +0200

    util: add sysfs util to read usb properties

---
+++ util/sysfs/src/sysfs.cpp
@@ -0,0 +1,61 @@
+// Copyright 2021-2022 FLECS Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/sysfs/sysfs.h"
+
+#include <fstream>
+
+namespace FLECS {
+namespace sysfs {
+
+#ifndef FLECS_UNIT_TEST
+constexpr auto base_path = "/sys/bus/usb/devices/";
+#else
+constexpr auto base_path = "flecs-sysfs/";
+#endif
+
+namespace {
+auto read_file(std::string_view path) //
+    -> std::optional<std::string>
+{
+    auto file = std::ifstream{path.data()};
+    if (!file.good())
+    {
+        return {};
+    }
+
+    auto line = std::string{};
+    std::getline(file, line);
+    return line;
+}
+} // namespace
+
+auto usb_vendor(std::string_view port) //
+    -> std::optional<std::string>
+{
+    const auto path = std::string{base_path}.append(port).append("/manufacturer");
+    return read_file(path);
+}
+
+auto usb_device(std::string_view port) //
+    -> std::optional<std::string>
+{
+    const auto path = std::string{base_path}.append(port).append("/product");
+
+    auto file = std::ifstream{path};
+    return read_file(path);
+}
+
+} // namespace sysfs
+} // namespace FLECS
---
+++ util/sysfs/sysfs.h
@@ -0,0 +1,33 @@
+// Copyright 2021-2022 FLECS Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef D79653EB_6825_4DCA_A8AD_CFC77A04CACF
+#define D79653EB_6825_4DCA_A8AD_CFC77A04CACF
+
+#include <optional>
+#include <string>
+
+namespace FLECS {
+namespace sysfs {
+
+auto usb_vendor(std::string_view port) //
+    -> std::optional<std::string>;
+
+auto usb_device(std::string_view port) //
+    -> std::optional<std::string>;
+
+} // namespace sysfs
+} // namespace FLECS
+
+#endif /* D79653EB_6825_4DCA_A8AD_CFC77A04CACF */
