4b9eb8d4766baad9463f13bcb534963b6425e7a0
Alexander Reichert <alex@flecs.tech>
2022-08-17 14:48:32 +0200

    docker: add Docker images for FLECS core

---
+++ docker/flecs/fs/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copyright 2021-2022 FLECS Technologies GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PATH=/sbin:/usr/sbin:/bin:/usr/bin:/opt/flecs/bin
+
+containerd >/tmp/containerd.log 2>&1 &
+dockerd >/tmp/dockerd.log 2>&1 &
+
+# wait for Docker daemon to be ready
+while ! docker version >/dev/null 2>&1; do
+    sleep 1
+done
+
+# load FLECS system app images
+docker load --input /opt/flecs/assets/tech.flecs.mqtt-bridge_1.3.0-porpoise_*.tar.gz >/dev/null
+docker load --input /opt/flecs/assets/tech.flecs.service-mesh_1.3.0-porpoise_*.tar.gz >/dev/null
+
+flecsd
