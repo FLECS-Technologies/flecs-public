435d34a0f12ba52c19f3946c6f5bf42187e9e581
Alexander Reichert <alex@flecs.tech>
2022-12-21 16:40:04 +0100

    daemon: refactor marketplace module
    - redirect to v2 api
    - improve test coverage

--- daemon/modules/marketplace/test/test_marketplace.cpp
+++ daemon/modules/marketplace/test/test_marketplace.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cpr/cpr.h>
+
 #include "gtest/gtest.h"
 #include "marketplace/marketplace.h"
 
@@ -20,43 +22,130 @@ class module_marketplace_test_t : public FLECS::module_marketplace_t
 public:
     module_marketplace_test_t() = default;
 
+    auto do_init() //
+        -> void override
+    {
+        return FLECS::module_marketplace_t::do_init();
+    }
+    auto do_deinit() //
+        -> void override
+    {
+        return FLECS::module_marketplace_t::do_deinit();
+    }
+
     auto login(std::string user, std::string token)
     {
         return FLECS::module_marketplace_t::login(std::move(user), std::move(token));
     }
 
     auto logout(std::string_view user) { return FLECS::module_marketplace_t::logout(std::move(user)); }
+
+    auto& user() const noexcept { return FLECS::module_marketplace_t::user(); }
+    auto& token() const noexcept { return FLECS::module_marketplace_t::token(); }
 };
 
-TEST(module_marketplace, login)
+class test_api_t
 {
-    const auto user = "testuser";
-    const auto token = "abcdef-1234-5678-XYZ";
-    const auto out_expected = std::string{"{\"additionalInfo\":\"OK\"}"};
+public:
+    test_api_t()
+        : _{}
+    {}
 
-    auto mod = module_marketplace_test_t{};
+    auto start() //
+        -> void
+    {
+        _ = FLECS::flecs_api_t::instance()
+                .app()
+                .loglevel(crow::LogLevel::Critical)
+                .bindaddr("127.0.0.1")
+                .port(18951)
+                .run_async();
+        FLECS::flecs_api_t::instance().app().wait_for_server_start();
+    }
 
-    const auto res = mod.login(user, token);
+    auto stop() //
+        -> void
+    {
+        FLECS::flecs_api_t::instance().app().stop();
+        _.wait();
+    }
 
-    ASSERT_EQ(res.code, crow::status::OK);
-    ASSERT_EQ(res.body, out_expected);
-    ASSERT_EQ(mod.user(), user);
-    ASSERT_EQ(mod.token(), token);
+private:
+    std::future<void> _;
+};
+
+static constexpr auto user = "testuser";
+static constexpr auto token = "abcdef-1234-5678-XYZ";
+
+static auto api = test_api_t{};
+static auto uut = module_marketplace_test_t{};
+
+TEST(marketplace, init)
+{
+    uut.do_init();
+    api.start();
 }
 
-TEST(module_marketplace, logout)
+TEST(marketplace, login)
 {
-    const auto user = "testuser";
-    const auto token = "abcdef-1234-5678-XYZ";
-    const auto out_expected = std::string{"{\"additionalInfo\":\"OK\"}"};
+    const auto post_json = FLECS::json_t({{"user", user}, {"token", token}});
 
-    auto mod = module_marketplace_test_t{};
+    auto res = cpr::Post(
+        cpr::Url{"http://127.0.0.1:18951/marketplace/login"},
+        cpr::Header{{{"Content-Type"}, {"application/json"}}},
+        cpr::Body{post_json.dump()},
+        cpr::Redirect{false});
 
-    (void)mod.login(user, token);
-    const auto res = mod.logout(user);
+    ASSERT_EQ(res.status_code, cpr::status::HTTP_PERMANENT_REDIRECT);
+    ASSERT_EQ(res.header.find("Location")->second, "http://127.0.0.1:18951/v2/marketplace/login");
 
-    ASSERT_EQ(res.code, crow::status::OK);
-    ASSERT_EQ(res.body, out_expected);
-    ASSERT_TRUE(mod.user().empty());
-    ASSERT_TRUE(mod.token().empty());
+    using std::operator""s;
+
+    const auto out_expected = R"({"additionalInfo":"OK"})"s;
+
+    res = cpr::Post(
+        cpr::Url{"http://127.0.0.1:18951/v2/marketplace/login"},
+        cpr::Header{{{"Content-Type"}, {"application/json"}}},
+        cpr::Body{post_json.dump()});
+
+    ASSERT_EQ(res.status_code, cpr::status::HTTP_OK);
+    ASSERT_EQ(res.header.find("Content-Type")->second, "application/json");
+    ASSERT_EQ(res.text, out_expected);
+    ASSERT_EQ(uut.user(), user);
+    ASSERT_EQ(uut.token(), token);
+}
+
+TEST(marketplace, logout)
+{
+    const auto post_json = FLECS::json_t({{"user", user}});
+
+    auto res = cpr::Post(
+        cpr::Url{"http://127.0.0.1:18951/marketplace/logout"},
+        cpr::Header{{{"Content-Type"}, {"application/json"}}},
+        cpr::Body{post_json.dump()},
+        cpr::Redirect{false});
+
+    ASSERT_EQ(res.status_code, cpr::status::HTTP_PERMANENT_REDIRECT);
+    ASSERT_EQ(res.header.find("Location")->second, "http://127.0.0.1:18951/v2/marketplace/logout");
+
+    using std::operator""s;
+
+    const auto out_expected = R"({"additionalInfo":"OK"})"s;
+
+    res = cpr::Post(
+        cpr::Url{"http://127.0.0.1:18951/v2/marketplace/logout"},
+        cpr::Header{{{"Content-Type"}, {"application/json"}}},
+        cpr::Body{post_json.dump()});
+
+    ASSERT_EQ(res.status_code, cpr::status::HTTP_OK);
+    ASSERT_EQ(res.header.find("Content-Type")->second, "application/json");
+    ASSERT_EQ(res.text, out_expected);
+    ASSERT_TRUE(uut.user().empty());
+    ASSERT_TRUE(uut.token().empty());
+}
+
+TEST(marketplace, deinit)
+{
+    uut.do_deinit();
+    api.stop();
 }
