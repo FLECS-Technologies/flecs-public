e8f21bc88716c7c4aff73f68dbd8cc43464248f5
Alexander Reichert <alex@flecs.tech>
2023-01-13 17:56:33 +0100

    daemon: add distinct type for instance_id

---
+++ daemon/common/instance/src/instance_id.cpp
@@ -0,0 +1,100 @@
+// Copyright 2021-2023 FLECS Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "instance_id.h"
+
+#include "util/random/random.h"
+#include "util/string/format.h"
+
+namespace FLECS {
+
+instance_id_t::instance_id_t()
+    : _id{rnd()}
+{}
+
+instance_id_t::instance_id_t(std::uint32_t id)
+    : _id{id}
+{}
+
+auto instance_id_t::get() const noexcept //
+    -> std::uint32_t
+{
+    return _id;
+}
+
+auto instance_id_t::hex() const //
+    -> std::string
+{
+    return int_to_hex(get(), fmt::Lowercase, fmt::NoPrefix, fmt::LeadingZeroes);
+}
+
+auto instance_id_t::regenerate() //
+    -> void
+{
+    _id = rnd();
+}
+
+auto to_json(json_t& j, const instance_id_t& instance_id) //
+    -> void
+{
+    j = json_t({{"instanceId", instance_id.hex()}});
+}
+
+auto from_json(const json_t& j, instance_id_t& instance_id) //
+    -> void
+{
+    instance_id = instance_id_t{};
+    if (j.contains("instanceId")) {
+        auto id = std::stoul(j["instanceId"].get_ref<const std::string&>(), nullptr, 16);
+        instance_id = instance_id_t{static_cast<std::uint32_t>(id)};
+    }
+}
+
+auto operator<(const instance_id_t& lhs, const instance_id_t& rhs) //
+    -> bool
+{
+    return lhs.get() < rhs.get();
+}
+
+auto operator<=(const instance_id_t& lhs, const instance_id_t& rhs) //
+    -> bool
+{
+    return !(lhs > rhs);
+}
+
+auto operator>(const instance_id_t& lhs, const instance_id_t& rhs) //
+    -> bool
+{
+    return rhs < lhs;
+}
+
+auto operator>=(const instance_id_t& lhs, const instance_id_t& rhs) //
+    -> bool
+{
+    return !(lhs < rhs);
+}
+
+auto operator==(const instance_id_t& lhs, const instance_id_t& rhs) //
+    -> bool
+{
+    return lhs.get() == rhs.get();
+}
+
+auto operator!=(const instance_id_t& lhs, const instance_id_t& rhs) //
+    -> bool
+{
+    return !(lhs == rhs);
+}
+
+} // namespace FLECS
---
+++ daemon/common/instance/test/test_instance_id.cpp
@@ -0,0 +1,63 @@
+// Copyright 2021-2023 FLECS Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include "daemon/common/instance/instance_id.h"
+
+TEST(instance_id, init)
+{
+    const auto uut = FLECS::instance_id_t{13};
+    ASSERT_EQ(uut.get(), 13);
+}
+
+TEST(instance_id, regenerate)
+{
+    auto uut = FLECS::instance_id_t{};
+    const auto old_id = uut.get();
+    uut.regenerate();
+    ASSERT_NE(old_id, uut.get());
+}
+
+TEST(instance_id, hex)
+{
+    auto uut = FLECS::instance_id_t{12648430};
+    ASSERT_EQ(uut.hex(), "00c0ffee");
+}
+
+TEST(instance_id, compare)
+{
+    const auto uut1 = FLECS::instance_id_t{2};
+    const auto uut2 = FLECS::instance_id_t{3};
+
+    ASSERT_LT(uut1, uut2);
+    ASSERT_LE(uut1, uut2);
+    ASSERT_NE(uut1, uut2);
+    ASSERT_GE(uut2, uut1);
+    ASSERT_GT(uut2, uut1);
+
+    ASSERT_EQ(uut1, uut1);
+}
+
+TEST(instance_id, json)
+{
+    const auto uut1 = FLECS::instance_id_t{12648430};
+    const auto json_expected = R"({"instanceId":"00c0ffee"})";
+
+    auto json = FLECS::json_t(uut1);
+    ASSERT_EQ(json.dump(), json_expected);
+
+    auto uut2 = json.get<FLECS::instance_id_t>();
+    ASSERT_EQ(uut1, uut2);
+}
