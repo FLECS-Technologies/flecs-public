305d0756d43e3c557177aa857d2cd1e8ef67c7bc
Robert Hahn <RbHahn@protonmail.com>
2023-01-27 12:16:44 +0100

    added scripts to build test image and run tests

---
+++ test/scripts/build-image.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Copyright 2021-2023 FLECS Technologies GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ ${EUID} -gt 0 ]; then
+  echo "Script has to be run as root" 1>&2
+  exit 1
+fi
+
+for arg in "$@"; do
+  DOCKER_ARGS="${DOCKER_ARGS} --build-arg $arg"
+done
+
+DIRNAME=$(dirname $(readlink -f ${0}))/..
+
+docker buildx build \
+    --load \
+    --build-arg ARCH=amd64 \
+    --tag "flecs/flecs-test:latest" \
+    --file dockerfiles/Dockerfile.Debian-11 \
+    ${DOCKER_ARGS} ${DIRNAME}
+
+
+docker run \
+    -p 8951:8951 \
+    -p 8888:8888 \
+    -p 3000:3000 \
+    -p 8086:8086 \
+    --name flecs-test -it --rm --privileged flecs/flecs-test:latest
+
+    # core
+    # AnyViz
+    # Grafana
+    # InfluxDB
