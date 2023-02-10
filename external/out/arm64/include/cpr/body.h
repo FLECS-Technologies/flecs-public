e66212166c18504d19acd49f8cda4f2573ca3558
Alexander Reichert <alex@flecs.tech>
2023-02-10 14:07:59 +0100

    external: update libcpr to 1.10.0

--- external/out/arm64/include/cpr/body.h
+++ external/out/arm64/include/cpr/body.h
@@ -2,10 +2,10 @@
 #define CPR_BODY_H
 
 #include <exception>
+#include <fstream>
 #include <initializer_list>
 #include <string>
 #include <vector>
-#include <fstream>
 
 #include "cpr/buffer.h"
 #include "cpr/cprtypes.h"
@@ -15,30 +15,31 @@ namespace cpr {
 
 class Body : public StringHolder<Body> {
   public:
-    Body() : StringHolder<Body>() {}
+    Body() = default;
     // NOLINTNEXTLINE(google-explicit-constructor, hicpp-explicit-conversions)
-    Body(const std::string& body) : StringHolder<Body>(body) {}
+    Body(std::string body) : StringHolder<Body>(std::move(body)) {}
     // NOLINTNEXTLINE(google-explicit-constructor, hicpp-explicit-conversions)
-    Body(std::string&& body) : StringHolder<Body>(std::move(body)) {}
+    Body(std::string_view body) : StringHolder<Body>(body) {}
     // NOLINTNEXTLINE(google-explicit-constructor, hicpp-explicit-conversions)
     Body(const char* body) : StringHolder<Body>(body) {}
     Body(const char* str, size_t len) : StringHolder<Body>(str, len) {}
     Body(const std::initializer_list<std::string> args) : StringHolder<Body>(args) {}
+    // NOLINTNEXTLINE(google-explicit-constructor, cppcoreguidelines-pro-type-reinterpret-cast)
     Body(const Buffer& buffer) : StringHolder<Body>(reinterpret_cast<const char*>(buffer.data), static_cast<size_t>(buffer.datalen)) {}
+    // NOLINTNEXTLINE(google-explicit-constructor)
     Body(const File& file) {
         std::ifstream is(file.filepath, std::ifstream::binary);
-        if (is) {
-            is.seekg(0, is.end);
-            int length = is.tellg();
-            is.seekg(0, is.beg);
-            std::vector<char> buffer;
-            buffer.resize(length);
-            is.read(&buffer[0], length);
-            is.close();
-            str_ = std::string(buffer.begin(), buffer.end());
-        } else {
+        if (!is) {
             throw std::invalid_argument("Can't open the file for HTTP request body!");
         }
+
+        is.seekg(0, std::ios::end);
+        const std::streampos length = is.tellg();
+        is.seekg(0, std::ios::beg);
+        std::string buffer;
+        buffer.resize(static_cast<size_t>(length));
+        is.read(buffer.data(), length);
+        str_ = std::move(buffer);
     }
     Body(const Body& other) = default;
     Body(Body&& old) noexcept = default;
