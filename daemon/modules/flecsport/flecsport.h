e5bf6c644ed5abb83f9c1fb3bf860232d0bc15ae
Alexander Reichert <alex@flecs.tech>
2023-02-19 20:13:28 +0100

    flecsport: implement skeleton module and include in build

--- daemon/modules/flecsport/flecsport.h
+++ daemon/modules/flecsport/flecsport.h
@@ -11,3 +11,36 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#pragma once
+
+#include "module_base/module.h"
+
+namespace FLECS {
+
+namespace impl {
+class module_flecsport_t;
+} // namespace impl
+
+class module_flecsport_t FLECS_FINAL_UNLESS_TESTED : public module_t
+{
+public:
+    ~module_flecsport_t() override;
+
+protected:
+    friend class module_factory_t;
+
+    module_flecsport_t();
+
+    auto do_init() //
+        -> void override
+    {}
+
+    auto do_deinit() //
+        -> void override
+    {}
+
+    std::unique_ptr<impl::module_flecsport_t> _impl;
+};
+
+} // namespace FLECS
