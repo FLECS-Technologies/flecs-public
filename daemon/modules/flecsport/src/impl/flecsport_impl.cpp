97ffdfca58b4032b51342b220470b852976ae55d
Alexander Reichert <alex@flecs.tech>
2023-02-22 10:37:16 +0100

    flecsport: implement export of Apps and Instances

--- daemon/modules/flecsport/src/impl/flecsport_impl.cpp
+++ daemon/modules/flecsport/src/impl/flecsport_impl.cpp
@@ -14,12 +14,13 @@
 
 #include "impl/flecsport_impl.h"
 
-#include "common/app/app_key.h"
+#include "common/app/app.h"
 #include "common/instance/instance_id.h"
 #include "modules/apps/apps.h"
 #include "modules/factory/factory.h"
 #include "modules/instances/instances.h"
 #include "modules/jobs/jobs.h"
+#include "util/archive/archive.h"
 
 namespace FLECS {
 namespace impl {
@@ -64,12 +65,41 @@ auto module_flecsport_t::do_export_to_sync(
     return do_export_to(std::move(apps), std::move(instances), std::move(dest_dir), _);
 }
 auto module_flecsport_t::do_export_to(
-    std::vector<app_key_t> /*apps*/,
-    std::vector<instance_id_t> /*instances*/,
-    fs::path /*dest_dir*/,
-    job_progress_t& /*progress*/) //
+    std::vector<app_key_t> apps,
+    std::vector<instance_id_t> instances,
+    fs::path dest_dir,
+    job_progress_t& progress) //
     -> result_t
 {
+    progress.num_steps(apps.size() + instances.size() + 1);
+    for (auto& app_key : apps) {
+        progress.next_step("Exporting App " + to_string(app_key));
+        auto [res, message] = _apps_api->export_to(app_key, dest_dir / "apps");
+        if (res != 0) {
+            auto ec = std::error_code{};
+            fs::remove_all(dest_dir, ec);
+            return {res, message};
+        }
+    }
+    for (auto& instance_id : instances) {
+        progress.next_step("Exporting Instance " + instance_id.hex());
+        auto [res, message] = _instances_api->export_to(instance_id, dest_dir / "instances");
+        if (res != 0) {
+            auto ec = std::error_code{};
+            fs::remove_all(dest_dir, ec);
+            return {res, message};
+        }
+    }
+
+    progress.next_step("Creating compressed archive");
+    auto archive_name = fs::canonical(dest_dir).string() + ".tar.gz";
+    auto res = archive::compress(archive_name, {dest_dir}, dest_dir.parent_path());
+    if (res != 0) {
+        auto ec = std::error_code{};
+        fs::remove_all(dest_dir, ec);
+        return {res, "Could not create compressed archive"};
+    }
+
     return {0, {}};
 }
 
