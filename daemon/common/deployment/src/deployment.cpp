2cf90de1b60f735bfe6bb7092c1e64a49d090dd1
Lukas Simon <lukas@flecs.tech>
2024-02-08 13:38:20 +0100

    Extract calculation of base ip and subnet size into separate functions

--- daemon/common/deployment/src/deployment.cpp
+++ daemon/common/deployment/src/deployment.cpp
@@ -741,39 +741,52 @@ auto deployment_t::default_network_gateway() const //
     return do_default_network_gateway();
 }
 
+auto deployment_t::get_base_ip(std::string_view cidr_subnet) const //
+    -> std::optional<ip_addr_t>
+{
+    // parse a.b.c.d
+    // from beginning of line: (a.b.c.)(d/)
+    // e.g. 127.0.0.1/24 -> (127.0.0.)(1)/
+    const auto ip_regex = std::regex{R"((^(?:\d{1,3}\.){3}\d{1,3})\/)"};
+    auto m = std::cmatch{};
+    if (!std::regex_search(cidr_subnet.data(), m, ip_regex)) {
+        return {};
+    }
+    return ip_addr_t{m[1].str()};
+}
+
+auto deployment_t::get_subnet_size(std::string_view cidr_subnet) const //
+    -> std::optional<int>
+{
+    // parse /x
+    // until end of line: d/[0-32]
+    // e.g. 127.0.0.1/24 -> 1/24
+    const auto subnet_regex = std::regex{R"(\d\/([0-9]|[1][0-9]|[2][0-9]|[3][0-2])$)"};
+    auto m = std::cmatch{};
+    if (!std::regex_search(cidr_subnet.data(), m, subnet_regex) || m.size() < 2) {
+        return {};
+    }
+    return std::stoi(m[1].str());
+}
+
 auto deployment_t::generate_instance_ip(std::string_view cidr_subnet, std::string_view gateway) const //
     -> std::string
 {
-    // parse a.b.c.d
-    auto base_ip = ip_addr_t{};
-    {
-        // from beginning of line: (a.b.c.)(d/)
-        // e.g. 127.0.0.1/24 -> (127.0.0.)(1)/
-        const auto ip_regex = std::regex{R"((^(?:\d{1,3}\.){3}\d{1,3})\/)"};
-        auto m = std::cmatch{};
-        if (!std::regex_search(cidr_subnet.data(), m, ip_regex)) {
-            return {};
-        }
-        base_ip = ip_addr_t{m[1].str()};
+    auto base_ip = get_base_ip(cidr_subnet);
+    if (!base_ip.has_value()) {
+        return {};
     }
-    // parse /x
-    auto subnet_size = int{};
-    {
-        // until end of line: d/[0-32]
-        // e.g. 127.0.0.1/24 -> 1/24
-        const auto subnet_regex = std::regex{R"(\d\/([0-9]|[1][0-9]|[2][0-9]|[3][0-2])$)"};
-        auto m = std::cmatch{};
-        if (!std::regex_search(cidr_subnet.data(), m, subnet_regex) || m.size() < 2) {
-            return {};
-        }
-        subnet_size = std::stoi(m[1].str());
+
+    auto subnet_size = get_subnet_size(cidr_subnet);
+    if (!subnet_size.has_value()) {
+        return {};
     }
 
     // determine the last ip address that belongs to the subnet:
     // (~0u << subnet_size) creates an inverted bitmask (such as 0xff00),
     // which is again inverted (yielding e.g. 0x00ff) and or'ed with the base ip.
     // finally subtract 1 to exclude the network's broadcast address
-    const auto max_ip = ip_addr_t{(base_ip.addr_v4().s_addr | ~(~0u << subnet_size)) - 1};
+    const auto max_ip = ip_addr_t{(base_ip.value().addr_v4().s_addr | ~(~0u << subnet_size.value())) - 1};
 
     auto used_ips = std::set<ip_addr_t>{};
     if (!gateway.empty()) {
@@ -788,7 +801,7 @@ auto deployment_t::generate_instance_ip(std::string_view cidr_subnet, std::strin
     }
 
     // skip network address and host address
-    auto instance_ip = base_ip + 2;
+    auto instance_ip = base_ip.value() + 2;
 
     // search first unused address
     while (used_ips.count(instance_ip)) {
