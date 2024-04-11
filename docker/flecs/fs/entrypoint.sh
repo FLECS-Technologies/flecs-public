17a33a00c21ef162db39ef66ba9d6360ff743f01
Alexander Reichert <alex@flecs.tech>
2024-04-11 11:46:47 +0200

    Refactor package and Docker builds

--- docker/flecs/fs/entrypoint.sh
+++ docker/flecs/fs/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Copyright 2021-2023 FLECS Technologies GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,9 +14,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PATH=${PATH}:/opt/flecs/bin
+PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin
 
-# verify docker socket is ready
+containerd >/tmp/containerd.log 2>&1 &
+dockerd >/tmp/dockerd.log 2>&1 &
+
+# wait for Docker daemon to be ready
 while ! docker version >/dev/null 2>&1; do
     sleep 1
 done
