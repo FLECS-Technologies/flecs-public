e5955e77364c9a35edcc1c476467dad74486fce2
Lukas Simon <lukas@flecs.tech>
2024-07-11 16:33:36 +0200

    Use flecs-console-api as submodule with client implementation, download manifests via spell

--- flecs-core/src/lib.rs
+++ flecs-core/src/lib.rs
@@ -4,20 +4,5 @@ pub mod fsm;
 pub mod lore;
 mod relic;
 mod sorcerer;
-mod spell;
+pub mod spell;
 mod vault;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
