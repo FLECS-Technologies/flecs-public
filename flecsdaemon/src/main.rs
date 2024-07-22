030fab0f8f8ecf42b0b5e33533249812220cf8c4
Lukas Simon <lukas@flecs.tech>
2024-07-22 13:41:31 +0200

    Print flecsd version via config.toml and env

--- flecsdaemon/src/main.rs
+++ flecsdaemon/src/main.rs
@@ -1,3 +1,5 @@
+const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
+const CODENAME: Option<&str> = option_env!("CODENAME");
 fn main() {
-    println!("Hello, world!");
+    println!("Running flecsd version {}-{}", VERSION.unwrap_or("unknown"), CODENAME.unwrap_or("unknown"));
 }
