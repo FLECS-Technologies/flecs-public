ba7d4608417af0042e486076c15d00d1ce708a9c
Lukas Simon <lukas@flecs.tech>
2024-09-26 16:10:19 +0200

    api: Remove handling of /v2/system/version in C++ implementation

--- flecs/modules/version/test/test_version.cpp
+++ flecs/modules/version/test/test_version.cpp
@@ -44,22 +44,6 @@ TEST(module_version, init)
     flecs::flecs_api_t::instance().app().validate();
 }
 
-TEST(module_version, print_version)
-{
-    using std::operator""s;
-
-    auto req = crow::request{};
-    auto res = crow::response{};
-
-    const auto json_expected = flecs::json_t({{"core", FLECS_VERSION + "-"s + FLECS_GIT_SHA}, {"api", FLECS_API_VERSION}});
-
-    req.url = "/v2/system/version";
-    flecs::flecs_api_t::instance().app().handle(req, res);
-    ASSERT_EQ(res.code, crow::status::OK);
-    ASSERT_EQ(res.headers.find("Content-Type")->second, "application/json");
-    ASSERT_EQ(res.body, json_expected.dump());
-}
-
 TEST(module_version, deinit)
 {
     uut.do_deinit();
