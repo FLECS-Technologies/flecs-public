480d39f3956b02efa3ae87ed9a13b4ca03117df2
Lukas Simon <lukas@flecs.tech>
2024-10-09 13:21:22 +0200

    relic::serde: Serialize iterator with serde via new adapter

--- flecs-core/src/relic/mod.rs
+++ flecs-core/src/relic/mod.rs
@@ -5,3 +5,4 @@ pub use super::{Error, Result};
 /// Helper functions that provide async versions of [flecstract::tar::extract] and [flecstract::tar::archive]
 pub mod async_flecstract;
 pub mod docker;
+pub mod serde;
