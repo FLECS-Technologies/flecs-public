a0c04f3a2a6d5a3415206059b55adf15593c17e1
Lukas Simon <lukas@flecs.tech>
2024-10-14 10:53:25 +0200

    vault::pouch: Adapt AppPouch to new apps concept

--- flecs-core/src/vault/pouch/app.rs
+++ flecs-core/src/vault/pouch/app.rs
@@ -1,37 +1,67 @@
-use crate::vault::pouch::{AppKey, Pouch, VaultPouch};
+use crate::relic::device::usb::UsbDevice;
+use crate::vault::pouch::{AppKey, DeploymentId, Pouch, VaultPouch};
+use flecsd_axum_server::models;
+use flecsd_axum_server::models::InstanceStatus;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs;
+use std::net::IpAddr;
 use std::path::{Path, PathBuf};
 
-#[derive(Serialize, Deserialize, Debug)]
-pub enum AppStatus {
-    #[serde(rename = "not installed")]
-    NotInstalled,
-    #[serde(rename = "manifest downloaded")]
-    ManifestDownloaded,
-    #[serde(rename = "token acquired")]
-    TokenAcquired,
-    #[serde(rename = "image downloaded")]
-    ImageDownloaded,
-    #[serde(rename = "installed")]
-    Installed,
-    #[serde(rename = "removed")]
-    Removed,
-    #[serde(rename = "purged")]
-    Purged,
-    #[serde(rename = "orphaned")]
-    Orphaned,
+const APPS_FILE_NAME: &str = "apps.json";
+pub type AppStatus = models::AppStatus;
+
+pub type InstanceId = String;
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct PortRange {
+    start: u16,
+    end: u16,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(untagged)]
+pub enum PortMapping {
+    Range { from: PortRange, to: PortRange },
+    Single { from: u16, to: u16 },
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct EnvironmentVariable {
+    name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    value: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct Network {
+    ip_addr: IpAddr,
+    mac_addr: String,
+    name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct Instance {
+    id: InstanceId,
+    name: String,
+    desired: InstanceStatus,
+    networks: Vec<Network>,
+    environment: Vec<EnvironmentVariable>,
+    ports: Vec<PortMapping>,
+    usb_devices: Vec<UsbDevice>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct AppData {
+    desired: AppStatus,
+    instances: Vec<Instance>,
 }
 
 // TODO: Implement version handling
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct App {
     app_key: AppKey,
-    status: AppStatus,
-    desired: AppStatus,
-    installed_size: i64,
+    properties: HashMap<DeploymentId, AppData>,
 }
 
 pub struct AppPouch {
@@ -53,15 +83,15 @@ impl Pouch for AppPouch {
 
 impl VaultPouch for AppPouch {
     fn close(&mut self) -> crate::vault::Result<()> {
+        let file = fs::File::create(self.path.join(APPS_FILE_NAME))?;
         let content: Vec<_> = self.apps.values().collect();
-        let content = serde_json::to_string(&content)?;
-        fs::write(self.path.join("apps.json"), content)?;
+        serde_json::to_writer_pretty(file, &content)?;
         Ok(())
     }
 
     fn open(&mut self) -> crate::vault::Result<()> {
-        let session_file = fs::read_to_string(self.path.join("apps.json"))?;
-        let apps: Vec<App> = serde_json::from_str(&session_file)?;
+        let file = fs::File::open(self.path.join(APPS_FILE_NAME))?;
+        let apps: Vec<App> = serde_json::from_reader(file)?;
         self.apps = apps
             .into_iter()
             .map(|app| (app.app_key.clone(), app))
@@ -81,5 +111,184 @@ impl AppPouch {
 
 #[cfg(test)]
 mod tests {
-    // TODO: Test AppPouch
+    use super::*;
+    use serde_json::Value;
+    use std::fs;
+    use std::net::Ipv4Addr;
+    use std::path::Path;
+    use std::str::FromStr;
+
+    const TEST_PATH: &str = "/tmp/flecs-tests/app-pouch/";
+
+    fn prepare_path(path: &Path) {
+        println!("Preparing {:?}", path);
+        let _ = fs::remove_dir_all(path);
+        assert!(!path.try_exists().unwrap());
+        fs::create_dir_all(path).unwrap();
+        assert!(path.try_exists().unwrap());
+    }
+
+    fn create_test_json() -> Value {
+        serde_json::json!([
+            {
+                "app_key": {
+                    "name": "test-app-1",
+                    "version": "1.2.3"
+                },
+                "properties": {
+                    "test-deployment-id-1": {
+                        "desired": "installed",
+                        "instances": [
+                            {
+                                "id": "test-instance-id-1",
+                                "name": "Some Test Instance #1",
+                                "desired": "running",
+                                "networks": [
+                                    {
+                                        "ip_addr": "12.34.56.78",
+                                        "mac_addr": "60:2D:2F:45:BA:72",
+                                        "name": "Some Test Network #1"
+                                    }
+                                ],
+                                "environment": [
+                                    {
+                                        "name": "VariableWithValue",
+                                        "value": "TestValue"
+                                    },
+                                    {
+                                        "name": "VariableWithoutValue"
+                                    }
+                                ],
+                                "ports": [
+                                    {
+                                        "from": 10,
+                                        "to": 20
+                                    },
+                                    {
+                                        "from": {
+                                            "start": 2000,
+                                            "end": 3000,
+                                        },
+                                        "to": {
+                                            "start": 5000,
+                                            "end": 6000,
+                                        }
+                                    }
+                                ],
+                                "usb_devices": [
+                                    {
+                                        "vid": 123,
+                                        "pid": 456,
+                                        "port": "test-port-1",
+                                        "device": "test-device-1",
+                                        "vendor": "test-vendor-1"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            }
+        ])
+    }
+
+    fn create_test_app() -> App {
+        App {
+            app_key: AppKey {
+                name: "test-app-1".to_string(),
+                version: "1.2.3".to_string(),
+            },
+            properties: HashMap::from([(
+                "test-deployment-id-1".to_string(),
+                AppData {
+                    desired: AppStatus::Installed,
+                    instances: vec![Instance {
+                        id: "test-instance-id-1".to_string(),
+                        name: "Some Test Instance #1".to_string(),
+                        desired: InstanceStatus::Running,
+                        networks: vec![Network {
+                            ip_addr: IpAddr::V4(Ipv4Addr::from_str("12.34.56.78").unwrap()),
+                            mac_addr: "60:2D:2F:45:BA:72".to_string(),
+                            name: "Some Test Network #1".to_string(),
+                        }],
+                        environment: vec![
+                            EnvironmentVariable {
+                                name: "VariableWithValue".to_string(),
+                                value: Some("TestValue".to_string()),
+                            },
+                            EnvironmentVariable {
+                                name: "VariableWithoutValue".to_string(),
+                                value: None,
+                            },
+                        ],
+                        ports: vec![
+                            PortMapping::Single { from: 10, to: 20 },
+                            PortMapping::Range {
+                                from: PortRange {
+                                    start: 2000,
+                                    end: 3000,
+                                },
+                                to: PortRange {
+                                    start: 5000,
+                                    end: 6000,
+                                },
+                            },
+                        ],
+                        usb_devices: vec![UsbDevice {
+                            vid: 123,
+                            pid: 456,
+                            port: "test-port-1".to_string(),
+                            device: "test-device-1".to_string(),
+                            vendor: "test-vendor-1".to_string(),
+                        }],
+                    }],
+                },
+            )]),
+        }
+    }
+
+    #[test]
+    fn open_app_pouch() {
+        let path = Path::new(TEST_PATH).join("open_pouch");
+        prepare_path(&path);
+        let json = create_test_json();
+        fs::write(
+            path.join(APPS_FILE_NAME),
+            serde_json::to_string_pretty(&json).unwrap(),
+        )
+        .unwrap();
+        let mut app_pouch = AppPouch {
+            apps: HashMap::default(),
+            path,
+        };
+        app_pouch.open().unwrap();
+    }
+
+    #[test]
+    fn close_app_pouch() {
+        let path = Path::new(TEST_PATH).join("close_pouch");
+        prepare_path(&path);
+        let json = create_test_json();
+        let app = create_test_app();
+        let mut app_pouch = AppPouch {
+            apps: HashMap::from([(app.app_key.clone(), app)]),
+            path: path.clone(),
+        };
+        app_pouch.close().unwrap();
+        let file = fs::File::open(path.join(APPS_FILE_NAME)).unwrap();
+        let content: Value = serde_json::from_reader(file).unwrap();
+        assert_eq!(content, json);
+    }
+
+    #[test]
+    fn app_gems() {
+        let app = create_test_app();
+        let gems = HashMap::from([(app.app_key.clone(), app)]);
+        let mut app_pouch = AppPouch {
+            apps: gems.clone(),
+            path: PathBuf::from(TEST_PATH),
+        };
+        assert_eq!(&gems, app_pouch.gems());
+        assert_eq!(&gems, app_pouch.gems_mut());
+    }
 }
