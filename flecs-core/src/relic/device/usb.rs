e2b8f4bce05b87d69080d33fe67408ed08e009d6
Lukas Simon <lukas@flecs.tech>
2024-12-13 10:53:40 +0100

    gem::instance: Use devices on instance start

--- flecs-core/src/relic/device/usb.rs
+++ flecs-core/src/relic/device/usb.rs
@@ -2,8 +2,14 @@ use rusb::Device;
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
 use std::fs;
+use std::str::FromStr;
 use thiserror::Error;
 use usb_ids::FromId;
+
+#[cfg(not(test))]
+const USB_DEVICE_PATH: &str = "/sys/bus/usb/devices/";
+#[cfg(test)]
+const USB_DEVICE_PATH: &str = "/tmp/flecs-tests/sys/bus/usb/devices/";
 
 #[derive(Error, Debug)]
 pub enum Error {
@@ -63,23 +69,131 @@ impl<T: rusb::UsbContext> TryFrom<Device<T>> for UsbDevice {
 fn get_vendor(vid: u16, port: &str) -> Option<String> {
     usb_ids::Vendor::from_id(vid)
         .map(|v| v.name().to_string())
-        .or_else(|| fs::read_to_string(format!("/sys/bus/usb/devices/{port}/manufacturer")).ok())
+        .or_else(|| get_manufacturer(port))
 }
 
 fn get_device_name(vid: u16, pid: u16, port: &str) -> Option<String> {
     usb_ids::Device::from_vid_pid(vid, pid)
         .map(|device| device.name().to_string())
-        .or_else(|| fs::read_to_string(format!("/sys/bus/usb/devices/{port}/product")).ok())
+        .or_else(|| get_product(port))
+}
+
+fn get_product(port: &str) -> Option<String> {
+    get_usb_value("product", port)
+}
+
+fn get_manufacturer(port: &str) -> Option<String> {
+    get_usb_value("manufacturer", port)
+}
+
+fn get_bus_num(port: &str) -> Option<u16> {
+    let bus_num = get_usb_value("busnum", port)?;
+    u16::from_str(&bus_num).ok()
+}
+
+fn get_dev_num(port: &str) -> Option<u16> {
+    let dev_num = get_usb_value("devnum", port)?;
+    u16::from_str(&dev_num).ok()
+}
+
+fn get_usb_value(value_name: &str, port: &str) -> Option<String> {
+    fs::read_to_string(format!("{USB_DEVICE_PATH}{port}/{value_name}")).ok()
+}
+
+impl UsbDevice {
+    pub fn get_bus_num(&self) -> Option<u16> {
+        get_bus_num(&self.port)
+    }
+
+    pub fn get_dev_num(&self) -> Option<u16> {
+        get_dev_num(&self.port)
+    }
 }
 
 #[cfg(test)]
-mod tests {
+pub mod tests {
     use super::*;
+    use std::path::{Path, PathBuf};
+
+    pub fn prepare_usb_device_test_path(test_name: &str) -> PathBuf {
+        let path = Path::new(USB_DEVICE_PATH).join(test_name);
+        println!("Preparing {:?}", path);
+        let _ = fs::remove_dir_all(&path);
+        assert!(!path.try_exists().unwrap());
+        fs::create_dir_all(&path).unwrap();
+        assert!(path.try_exists().unwrap());
+        path
+    }
+
     #[test]
-    #[ignore]
-    fn print_devices() {
-        for device in read_usb_devices().unwrap() {
-            println!("{device:?}");
-        }
+    fn get_product_ok() {
+        let path = prepare_usb_device_test_path("get_product_ok");
+        fs::write(path.join("product"), b"Test Product 9000").unwrap();
+        assert_eq!(
+            get_product("get_product_ok"),
+            Some("Test Product 9000".to_string())
+        );
+    }
+
+    #[test]
+    fn get_product_none() {
+        prepare_usb_device_test_path("get_product_none");
+        assert_eq!(get_product("get_product_none"), None);
+    }
+
+    #[test]
+    fn get_manufacturer_ok() {
+        let path = prepare_usb_device_test_path("get_manufacturer_ok");
+        fs::write(path.join("manufacturer"), b"Test Manufacturer Inc").unwrap();
+        assert_eq!(
+            get_manufacturer("get_manufacturer_ok"),
+            Some("Test Manufacturer Inc".to_string())
+        );
+    }
+
+    #[test]
+    fn get_manufacturer_none() {
+        prepare_usb_device_test_path("get_manufacturer_none");
+        assert_eq!(get_manufacturer("get_manufacturer_none"), None);
+    }
+
+    #[test]
+    fn get_bus_num_ok() {
+        let path = prepare_usb_device_test_path("get_bus_num_ok");
+        fs::write(path.join("busnum"), b"123").unwrap();
+        assert_eq!(get_bus_num("get_bus_num_ok"), Some(123));
+    }
+
+    #[test]
+    fn get_bus_num_none() {
+        prepare_usb_device_test_path("get_bus_num_none");
+        assert_eq!(get_product("get_bus_num_none"), None);
+    }
+
+    #[test]
+    fn get_bus_num_err() {
+        let path = prepare_usb_device_test_path("get_bus_num_err");
+        fs::write(path.join("busnum"), b"invalid number").unwrap();
+        assert_eq!(get_product("get_bus_num_err"), None);
+    }
+
+    #[test]
+    fn get_dev_num_ok() {
+        let path = prepare_usb_device_test_path("get_dev_num_ok");
+        fs::write(path.join("devnum"), b"123").unwrap();
+        assert_eq!(get_dev_num("get_dev_num_ok"), Some(123));
+    }
+
+    #[test]
+    fn get_dev_num_none() {
+        prepare_usb_device_test_path("get_dev_num_none");
+        assert_eq!(get_product("get_dev_num_none"), None);
+    }
+
+    #[test]
+    fn get_dev_num_err() {
+        let path = prepare_usb_device_test_path("get_dev_num_err");
+        fs::write(path.join("devnum"), b"invalid number").unwrap();
+        assert_eq!(get_product("get_dev_num_err"), None);
     }
 }
