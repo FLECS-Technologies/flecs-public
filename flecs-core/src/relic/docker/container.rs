fe540d2a0cbbcf13cbebff5e11065fa11bd88ca8
Lukas Simon <lukas@flecs.tech>
2024-12-17 16:15:14 +0100

    relic::docker: Unit test list containers

--- flecs-core/src/relic/docker/container.rs
+++ flecs-core/src/relic/docker/container.rs
@@ -900,6 +900,186 @@ mod tests {
     use super::*;
     use bollard::ClientVersion;
 
+    fn example_container_list() -> serde_json::Value {
+        // Example taken from https://docs.docker.com/reference/api/engine/version/v1.46/#tag/Container/operation/ContainerList
+        serde_json::json!([
+          {
+            "Id": "8dfafdbc3a40",
+            "Names": [
+              "/boring_feynman"
+            ],
+            "Image": "ubuntu:latest",
+            "ImageID": "d74508fb6632491cea586a1fd7d748dfc5274cd6fdfedee309ecdcbc2bf5cb82",
+            "Command": "echo 1",
+            "Created": 1367854155,
+            "State": "Exited",
+            "Status": "Exit 0",
+            "Ports": [
+              {
+                "PrivatePort": 2222,
+                "PublicPort": 3333,
+                "Type": "tcp"
+              }
+            ],
+            "Labels": {
+              "com.example.vendor": "Acme",
+              "com.example.license": "GPL",
+              "com.example.version": "1.0"
+            },
+            "SizeRw": 12288,
+            "SizeRootFs": 0,
+            "HostConfig": {
+              "NetworkMode": "default",
+              "Annotations": {
+                "io.kubernetes.docker.type": "container"
+              }
+            },
+            "NetworkSettings": {
+              "Networks": {
+                "bridge": {
+                  "NetworkID": "7ea29fc1412292a2d7bba362f9253545fecdfa8ce9a6e37dd10ba8bee7129812",
+                  "EndpointID": "2cdc4edb1ded3631c81f57966563e5c8525b81121bb3706a9a9a3ae102711f3f",
+                  "Gateway": "172.17.0.1",
+                  "IPAddress": "172.17.0.2",
+                  "IPPrefixLen": 16,
+                  "IPv6Gateway": "",
+                  "GlobalIPv6Address": "",
+                  "GlobalIPv6PrefixLen": 0,
+                  "MacAddress": "02:42:ac:11:00:02"
+                }
+              }
+            },
+            "Mounts": [
+              {
+                "Name": "fac362...80535",
+                "Source": "/data",
+                "Destination": "/data",
+                "Driver": "local",
+                "Mode": "ro,Z",
+                "RW": false,
+                "Propagation": ""
+              }
+            ]
+          },
+          {
+            "Id": "9cd87474be90",
+            "Names": [
+              "/coolName"
+            ],
+            "Image": "ubuntu:latest",
+            "ImageID": "d74508fb6632491cea586a1fd7d748dfc5274cd6fdfedee309ecdcbc2bf5cb82",
+            "Command": "echo 222222",
+            "Created": 1367854155,
+            "State": "Exited",
+            "Status": "Exit 0",
+            "Ports": [],
+            "Labels": {},
+            "SizeRw": 12288,
+            "SizeRootFs": 0,
+            "HostConfig": {
+              "NetworkMode": "default",
+              "Annotations": {
+                "io.kubernetes.docker.type": "container",
+                "io.kubernetes.sandbox.id": "3befe639bed0fd6afdd65fd1fa84506756f59360ec4adc270b0fdac9be22b4d3"
+              }
+            },
+            "NetworkSettings": {
+              "Networks": {
+                "bridge": {
+                  "NetworkID": "7ea29fc1412292a2d7bba362f9253545fecdfa8ce9a6e37dd10ba8bee7129812",
+                  "EndpointID": "88eaed7b37b38c2a3f0c4bc796494fdf51b270c2d22656412a2ca5d559a64d7a",
+                  "Gateway": "172.17.0.1",
+                  "IPAddress": "172.17.0.8",
+                  "IPPrefixLen": 16,
+                  "IPv6Gateway": "",
+                  "GlobalIPv6Address": "",
+                  "GlobalIPv6PrefixLen": 0,
+                  "MacAddress": "02:42:ac:11:00:08"
+                }
+              }
+            },
+            "Mounts": []
+          },
+          {
+            "Id": "3176a2479c92",
+            "Names": [
+              "/sleepy_dog"
+            ],
+            "Image": "ubuntu:latest",
+            "ImageID": "d74508fb6632491cea586a1fd7d748dfc5274cd6fdfedee309ecdcbc2bf5cb82",
+            "Command": "echo 3333333333333333",
+            "Created": 1367854154,
+            "State": "Exited",
+            "Status": "Exit 0",
+            "Ports": [],
+            "Labels": {},
+            "SizeRw": 12288,
+            "SizeRootFs": 0,
+            "HostConfig": {
+              "NetworkMode": "default",
+              "Annotations": {
+                "io.kubernetes.image.id": "d74508fb6632491cea586a1fd7d748dfc5274cd6fdfedee309ecdcbc2bf5cb82",
+                "io.kubernetes.image.name": "ubuntu:latest"
+              }
+            },
+            "NetworkSettings": {
+              "Networks": {
+                "bridge": {
+                  "NetworkID": "7ea29fc1412292a2d7bba362f9253545fecdfa8ce9a6e37dd10ba8bee7129812",
+                  "EndpointID": "8b27c041c30326d59cd6e6f510d4f8d1d570a228466f956edf7815508f78e30d",
+                  "Gateway": "172.17.0.1",
+                  "IPAddress": "172.17.0.6",
+                  "IPPrefixLen": 16,
+                  "IPv6Gateway": "",
+                  "GlobalIPv6Address": "",
+                  "GlobalIPv6PrefixLen": 0,
+                  "MacAddress": "02:42:ac:11:00:06"
+                }
+              }
+            },
+            "Mounts": []
+          },
+          {
+            "Id": "4cb07b47f9fb",
+            "Names": [
+              "/running_cat"
+            ],
+            "Image": "ubuntu:latest",
+            "ImageID": "d74508fb6632491cea586a1fd7d748dfc5274cd6fdfedee309ecdcbc2bf5cb82",
+            "Command": "echo 444444444444444444444444444444444",
+            "Created": 1367854152,
+            "State": "Exited",
+            "Status": "Exit 0",
+            "Ports": [],
+            "Labels": {},
+            "SizeRw": 12288,
+            "SizeRootFs": 0,
+            "HostConfig": {
+              "NetworkMode": "default",
+              "Annotations": {
+                "io.kubernetes.config.source": "api"
+              }
+            },
+            "NetworkSettings": {
+              "Networks": {
+                "bridge": {
+                  "NetworkID": "7ea29fc1412292a2d7bba362f9253545fecdfa8ce9a6e37dd10ba8bee7129812",
+                  "EndpointID": "d91c7b2f0644403d7ef3095985ea0e2370325cd2332ff3a3225c4247328e66e9",
+                  "Gateway": "172.17.0.1",
+                  "IPAddress": "172.17.0.5",
+                  "IPPrefixLen": 16,
+                  "IPv6Gateway": "",
+                  "GlobalIPv6Address": "",
+                  "GlobalIPv6PrefixLen": 0,
+                  "MacAddress": "02:42:ac:11:00:05"
+                }
+              }
+            },
+            "Mounts": []
+          }
+        ])
+    }
+
     pub async fn create_test_server_and_config() -> (mockito::ServerGuard, Arc<Docker>) {
         let server = mockito::Server::new_async().await;
         let client = Docker::connect_with_http(
@@ -1036,6 +1216,42 @@ mod tests {
         )
         .await
         .is_err());
+        mock.assert_async().await;
+    }
+
+    #[tokio::test]
+    async fn list_container_ok() {
+        let (mut mock_server, client) = create_test_server_and_config().await;
+        let body = serde_json::to_vec(&example_container_list()).unwrap();
+        let mock = mock_server
+            .mock("GET", "/containers/json")
+            .with_status(201)
+            .with_body(&body)
+            .create_async()
+            .await;
+        let containers = list(client, Option::<ListContainersOptions<&str>>::None)
+            .await
+            .unwrap();
+        assert_eq!(containers.len(), 4);
+        mock.assert_async().await;
+    }
+
+    #[tokio::test]
+    async fn list_container_err() {
+        let (mut mock_server, client) = create_test_server_and_config().await;
+        let body = serde_json::to_vec(&serde_json::json!({
+            "message": "Something went wrong."
+        }))
+        .unwrap();
+        let mock = mock_server
+            .mock("GET", "/containers/json")
+            .with_status(500)
+            .with_body(&body)
+            .create_async()
+            .await;
+        assert!(list(client, Option::<ListContainersOptions<&str>>::None,)
+            .await
+            .is_err());
         mock.assert_async().await;
     }
 
