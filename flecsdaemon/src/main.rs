0766bd0884d88dd7a3f815408ec6a0cac457036f
Lukas Simon <lukas@flecs.tech>
2025-01-14 10:01:19 +0100

    flecsdaemon: Start server in main

--- flecsdaemon/src/main.rs
+++ flecsdaemon/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+use tracing::info;
+
+const FLECSD_SOCKET: &str = "/run/flecs/flecsd.sock";
+
+#[tokio::main]
+async fn main() -> flecs_core::fsm::Result<()> {
+    flecs_core::fsm::init_backtracing();
+    flecs_core::fsm::init_tracing();
+    info!("Starting rust server listening on {FLECSD_SOCKET}");
+    flecs_core::fsm::server(PathBuf::from(FLECSD_SOCKET)).await
 }
