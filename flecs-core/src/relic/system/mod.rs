4d356323c6d59a4b1d38af603639a20468d2f2ac
Lukas Simon <lukas@flecs.tech>
2025-04-02 14:59:06 +0200

    relic::system: Implement reading of hostname

--- flecs-core/src/relic/system/mod.rs
+++ flecs-core/src/relic/system/mod.rs
@@ -1,3 +1,20 @@
 pub mod info;
 
 pub use super::{Error, Result};
+use libc::c_char;
+use std::ffi::CStr;
+use std::mem::MaybeUninit;
+
+pub fn hostname() -> std::io::Result<String> {
+    const BUFFER_LEN: usize = 256;
+    let mut buffer: MaybeUninit<[c_char; BUFFER_LEN]> = MaybeUninit::uninit();
+    if unsafe { libc::gethostname(buffer.as_mut_ptr() as *mut c_char, BUFFER_LEN) } != 0 {
+        Err(std::io::Error::last_os_error())
+    } else {
+        let hostname = unsafe {
+            let buffer = buffer.assume_init();
+            CStr::from_ptr(buffer.as_ptr())
+        };
+        Ok(hostname.to_string_lossy().to_string())
+    }
+}
