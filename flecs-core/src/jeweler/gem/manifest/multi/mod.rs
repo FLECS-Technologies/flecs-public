1dfff935f21298fe7297ff640a1487f444ca0386
Lukas Simon <lukas@flecs.tech>
2025-04-15 08:11:00 +0200

    gem::manifest::multi: Parse compose part of manifest

--- flecs-core/src/jeweler/gem/manifest/multi/mod.rs
+++ flecs-core/src/jeweler/gem/manifest/multi/mod.rs
@@ -1,11 +1,14 @@
 use crate::jeweler::GetAppKey;
 use crate::vault::pouch::AppKey;
+use docker_compose_types::Compose;
 use serde::Serialize;
 
 #[derive(Debug, PartialEq, Clone, Serialize)]
 pub struct AppManifestMulti {
     #[serde(skip_serializing)]
     pub key: AppKey,
+    #[serde(skip_serializing)]
+    pub compose: Compose,
     #[serde(flatten)]
     original: flecs_app_manifest::AppManifestMulti,
 }
@@ -20,7 +23,10 @@ impl TryFrom<flecs_app_manifest::AppManifestMulti> for AppManifestMulti {
     type Error = anyhow::Error;
 
     fn try_from(value: flecs_app_manifest::AppManifestMulti) -> Result<Self, Self::Error> {
+        let json_value = serde_json::Value::Object(value.deployment.compose.yaml.clone());
+        let compose = serde_json::from_value(json_value)?;
         Ok(Self {
+            compose,
             key: AppKey {
                 name: value.app.to_string(),
                 version: value.version.to_string(),
