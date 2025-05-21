2e2fe6fef7dd9b282642e3a6d90551ab074aadd5
Lukas Simon <lukas@flecs.tech>
2025-05-21 15:54:50 +0200

    relic::system: Use correct function to read CStr from byte array

--- flecs-core/src/relic/system/mod.rs
+++ flecs-core/src/relic/system/mod.rs
@@ -5,16 +5,26 @@ use libc::c_char;
 use std::ffi::CStr;
 use std::mem::MaybeUninit;
 
-pub fn hostname() -> std::io::Result<String> {
+use thiserror::Error;
+#[derive(Error, Debug)]
+pub enum ReadHostnameError {
+    #[error("Call to 'gethostname' failed: {0}")]
+    IO(#[from] std::io::Error),
+    #[error(transparent)]
+    CStr(#[from] std::ffi::FromBytesUntilNulError),
+    #[error(transparent)]
+    Utf8(#[from] std::str::Utf8Error),
+}
+
+pub fn hostname() -> Result<String, ReadHostnameError> {
     const BUFFER_LEN: usize = 256;
-    let mut buffer: MaybeUninit<[c_char; BUFFER_LEN]> = MaybeUninit::uninit();
+    let mut buffer: MaybeUninit<[u8; BUFFER_LEN]> = MaybeUninit::uninit();
     if unsafe { libc::gethostname(buffer.as_mut_ptr() as *mut c_char, BUFFER_LEN) } != 0 {
-        Err(std::io::Error::last_os_error())
+        Err(ReadHostnameError::IO(std::io::Error::last_os_error()))
     } else {
-        let hostname = unsafe {
-            let buffer = buffer.assume_init();
-            CStr::from_ptr(buffer.as_ptr())
-        };
-        Ok(hostname.to_string_lossy().to_string())
+        let hostname = unsafe { buffer.assume_init() };
+        let hostname = CStr::from_bytes_until_nul(&hostname)?;
+        let hostname = hostname.to_str()?;
+        Ok(hostname.to_string())
     }
 }
