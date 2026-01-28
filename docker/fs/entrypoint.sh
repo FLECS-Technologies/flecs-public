3c052a81527f6cbe0bdab050d30c5df9c489fa99
Lukas Simon <lukas@flecs.tech>
2026-01-28 07:45:47 +0100

    docker: Remove creation of flecs network and floxy from entrypoint

--- docker/fs/entrypoint.sh
+++ docker/fs/entrypoint.sh
@@ -14,116 +14,6 @@
 # limitations under the License.
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-FLOXY_CONTAINER_NAME=flecs-floxy
-# TODO: Change tag to latest before release
-FLOXY_IMAGE=flecspublic.azurecr.io/flecs/floxy:0
-
-create_network() {
-  NETWORK=flecs
-  # check if we have created network before
-  GATEWAY=`docker network inspect --format "{{range .IPAM.Config}}{{.Gateway}}{{end}}" "$NETWORK" 2>/dev/null`
-
-  # if network does not exist, create it
-  if [ -z "${GATEWAY}" ]; then
-    # list all in-use IP addresses
-    if ifconfig -a >/dev/null 2>&1; then
-      IPS=`ifconfig -a | sed -n -E 's/^[[:space:]]+inet ([0-9\.]+).+$/\1/p'`
-    elif ip addr >/dev/null 2>&1; then
-      IPS=`ip addr -a | sed -n -E 's/^[[:space:]]+inet ([0-9\.]+).+$/\1/p'`
-    else
-      echo "Warning: Cannot determine in-use IP addresses" 1>&2
-    fi
-    # try subnets 172.21.0.0/16 --> 172.31.0.0/16
-    SUBNETS=(21 22 23 24 25 26 27 28 29 30 31)
-    for SUBNET in "${SUBNETS[@]}"; do
-      # skip subnets that overlap with in-use IP addresses
-      SKIP_SUBNET=
-      for IP in ${IPS}; do
-        if [[ ${IP} == 172.${SUBNET}.* ]]; then
-          echo "${IP} collides with subnet 172.${SUBNET}.0.0/16 -- skipping"
-          SKIP_SUBNET="true"
-        fi
-      done
-      if [ ! -z "${SKIP_SUBNET}" ]; then
-        continue
-      fi
-      # try to create network as Docker bridge network
-      if docker network create --driver bridge --subnet "172.${SUBNET}.0.0/16" --gateway "172.${SUBNET}.0.1" "$NETWORK" >/dev/null 2>&1; then
-        GATEWAY="172.${SUBNET}.0.1"
-        echo "Created network '$NETWORK'"
-        break;
-      fi
-    done
-  else
-    echo "Reusing existing network '$NETWORK'"
-  fi
-
-  if [ -z "${GATEWAY}" ]; then
-    echo "Network '$NETWORK' does not exist and could not be created" 2>&1
-    exit 1
-  fi
-  local __resultvar=$1
-  local __value="${GATEWAY}"
-  eval $__resultvar="'$__value'"
-}
-
-determine_free_http_port() {
-    HTTP_PORTS=(80 8080 8000 none)
-    HTTP_PORTS_HEX=(0050 1F90 1F40 none)
-    for i in ${!HTTP_PORTS_HEX[*]}; do
-      if ! cat /proc/net/tcp /proc/net/tcp6 | grep -E ":${HTTP_PORTS_HEX[$i]} [0-9A-F]{8}:[0-9A-F]{4} 0A" >/dev/null 2>&1; then
-        break
-      fi
-    done
-
-    if [ "${HTTP_PORTS[$i]}" == "none" ]; then
-      echo "No free http port found in (" "${HTTP_PORTS[@]}" ") - exiting" 2>&1
-      exit 1
-    fi
-    local __resultvar=$1
-    local __value="${HTTP_PORTS[$j]}"
-    eval $__resultvar="'$__value'"
-}
-
-determine_free_https_port() {
-    HTTPS_PORTS=(443 8443 4443 none)
-    HTTPS_PORTS_HEX=(01BB 208C 114B none)
-    for j in ${!HTTPS_PORTS_HEX[*]}; do
-      if ! cat /proc/net/tcp /proc/net/tcp6 | grep -E ":${HTTPS_PORTS_HEX[$j]} [0-9A-F]{8}:[0-9A-F]{4} 0A" >/dev/null 2>&1; then
-        break
-      fi
-    done
-    if [ "${HTTPS_PORTS[$j]}" == "none" ]; then
-      echo "No free http port found in (" "${HTTPS_PORTS[@]}" ") - exiting" 2>&1
-      exit 1
-    fi
-    local __resultvar=$1
-    local __value="${HTTPS_PORTS[$j]}"
-    eval $__resultvar="'$__value'"
-}
-
-start_floxy() {
-  if [[ -z "$HTTPS_PORT" ]]; then
-      determine_free_https_port "HTTPS_PORT"
-  fi
-  if [[ -z "$HTTP_PORT" ]]; then
-      determine_free_http_port "HTTP_PORT"
-  fi
-
-  docker rm -f ${FLOXY_CONTAINER_NAME}
-  echo "Binding ${FLOXY_CONTAINER_NAME} to port ${HTTP_PORT}/http and ${HTTPS_PORT}/https"
-  docker pull "${FLOXY_IMAGE}" || true
-  docker create \
-        --name ${FLOXY_CONTAINER_NAME} \
-        --network host \
-        --volume flecs-floxy_certs:/etc/nginx/certs \
-        --volume flecs-floxy_data:/tmp/floxy \
-        --env "FLOXY_HTTP_PORT=${HTTP_PORT}" \
-        --env "FLOXY_HTTPS_PORT=${HTTPS_PORT}" \
-        --env "FLOXY_FLECS_GATEWAY=$1" \
-        "${FLOXY_IMAGE}"
-  docker start ${FLOXY_CONTAINER_NAME}
-}
 
 # Execute special entrypoint script not in common between flecs and flecs-slim
 if [ -f "./special_entrypoint.sh" ]; then
@@ -134,8 +24,5 @@ fi
 while ! docker version >/dev/null 2>&1; do
     sleep 1
 done
-
-create_network "GATEWAY"
-start_floxy "${GATEWAY}"
 
 exec flecsd $*
